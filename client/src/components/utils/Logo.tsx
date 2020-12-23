b40f5ad85eb3a55dc0042589127318e7ed4f37f1
Rz <rabira.hierpa@gmail.com>
2020-12-23 16:13:53 +0300

    added a component for logo

---
+++ client/src/components/utils/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Logo() {
+	return (
+		<img
+			src='../assets/img/logo/logo.svg'
+			alt='Rz Codes Logo'
+			width='50px'
+			height='50px'
+			style={{ paddingRight: '10px' }}
+		/>
+	);
+}
