50cbb93472f9bd37a24c5388145aeff04e536fb1
Rz <rabira.hierpa@gmail.com>
2020-12-23 17:10:29 +0300

    added side menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,18 @@ import { Grid } from '@material-ui/core';
 import React, { Component } from 'react';
 import Featured from './components/Featured';
 import Header from './components/Header';
+import SideMenu from './components/SideMenu';
 
 export default class App extends Component {
 	render() {
 		return (
-			<div>
+			<>
 				<Header />
+				<SideMenu />
 				<Grid container direction='row' justify='center' alignItems='center'>
 					<Featured />
 				</Grid>
-			</div>
+			</>
 		);
 	}
 }
