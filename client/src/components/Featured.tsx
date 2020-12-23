2a2cf923a24a0ccfc59cb4cb13e8feff56f4b383
Rz <rabira.hierpa@gmail.com>
2020-12-23 16:14:05 +0300

    added featured blog component

---
+++ client/src/components/Featured.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+export default function Featured() {
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<Container maxWidth='xl'></Container>
+		</React.Fragment>
+	);
+}
