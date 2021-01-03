40deaf7ffe5d4d1ba4c6965cd15e39d52f62861f
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-01-03 17:23:50 +0300

    restructured the app entry

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,24 @@
-import { CssBaseline, Grid, ThemeProvider } from '@material-ui/core';
 import React, { Component } from 'react';
-import Featured from './components/Featured';
-import Header from './components/Header';
-import SideMenu from './components/SideMenu';
-import RzTheme from './components/utils/RzTheme';
+import RzTheme from './theme';
+import { Router } from 'react-router-dom';
+import validate from 'validate.js';
+import validators from './common/validators';
+import { createBrowserHistory } from 'history';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 
+const browserHistory = createBrowserHistory();
+
+validate.validators = {
+	...validate.validators,
+	...validators,
+};
 export default class App extends Component {
 	render() {
 		return (
-			<React.Fragment>
-				<ThemeProvider theme={RzTheme}>
-					<CssBaseline />
-					<Header />
-					{/* <SideMenu /> */}
-					<Grid container direction='row' justify='center' alignItems='center'>
-						<Featured />
-					</Grid>
-				</ThemeProvider>
-			</React.Fragment>
+			<ThemeProvider theme={RzTheme}>
+				<CssBaseline />
+				<Router history={browserHistory}></Router>
+			</ThemeProvider>
 		);
 	}
 }
