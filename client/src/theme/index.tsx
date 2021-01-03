8c3fc6bf4ec6d016bce8ff87b9a34268e38bf5bc
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-01-03 17:22:43 +0300

    added typescript to theme files

--- client/src/theme/index.tsx
+++ client/src/theme/index.tsx
@@ -4,14 +4,14 @@ import palette from './palette';
 import typography from './typography';
 import overrides from './overrides';
 
-const theme = createMuiTheme({
-  palette,
-  typography,
-  overrides,
-  zIndex: {
-    appBar: 1200,
-    drawer: 1100
-  }
+const RzTheme = createMuiTheme({
+	palette,
+	typography,
+	overrides,
+	zIndex: {
+		appBar: 1200,
+		drawer: 1100,
+	},
 });
 
-export default theme;
+export default RzTheme;
